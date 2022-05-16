b93448d9991d74e9cdb3d98657e6f03d7c744605
chf <156704883@qq.com>
2022-05-16 22:38:50 +0800

    同步更新

---
+++ view/document/tpl/jstest.js
@@ -0,0 +1,8 @@
+layui.use(['toast'], function(){
+    var toast = layui.toast;
+                     
+    toast.success({title: 'Caution',message: 'You forgot important data',position: 'topRight'});
+    alert("121212");
+
+
+});
