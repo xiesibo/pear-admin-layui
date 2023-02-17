ff5c6c5d040adc6f4a293eec91352c5e783f3b4a
就眠儀式 <854085467@qq.com>
2023-02-18 00:07:15 +0800

    😄 release 3.30.0

--- component/pear/pear.js
+++ component/pear/pear.js
@@ -7,7 +7,7 @@ window.rootPath = (function (src) {
 
 layui.config({
 	base: rootPath + "module/",
-	version: "3.2.1"
+	version: "3.30.0"
 }).extend({
 	admin: "admin", 	// 框架布局组件
 	menu: "menu",		// 数据菜单组件
