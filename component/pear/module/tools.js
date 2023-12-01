19ee7803ba0ff6144830241f5cb1d1979907e067
就眠儀式 <854085467@qq.com>
2023-12-01 10:04:52 +0800

    修复 profile.html 控制台异常, 新增 tools 方法

--- component/pear/module/tools.js
+++ component/pear/module/tools.js
@@ -22,6 +22,18 @@ layui.define(['jquery', 'element'],
                     }, time)
                 }
             }
+
+            // image 转 base64
+            this.imageToBase64 = function (img) {
+                var canvas = document.createElement("canvas");
+                canvas.width = img.width;
+                canvas.height = img.height;
+                var ctx = canvas.getContext("2d");
+                ctx.drawImage(img, 0, 0, img.width, img.height);
+                var ext = img.src.substring(img.src.lastIndexOf(".") + 1).toLowerCase();
+                var dataURL = canvas.toDataURL("image/" + ext);
+                return dataURL;
+            }
         };
 
         exports('tools', tools);
